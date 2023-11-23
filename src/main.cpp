0ee82c07e53282c39c0a5b4f7d629c942db63b32
Luiz Henrique Silva Sampaio <luizhsampaio07@gmail.com>
2023-11-23 20:47:55 -0300

    Added vertex buffer

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,12 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+float vertices[] = {
+    -0.5f, -0.5f,
+    0.0f, 0.5f,
+    0.5f, -0.5f
+};
+
 int main() {
     if (!glfwInit()) {
         return -1;
@@ -16,13 +22,25 @@ int main() {
 
     gladLoadGL();
 
+    GLuint vertexBufferObject;
+    glGenBuffers(1, &vertexBufferObject);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexBufferObject);
+    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
     while (!glfwWindowShouldClose(window)) {
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glBindBuffer(GL_ARRAY_BUFFER, vertexBufferObject);
+
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         glfwSwapBuffers(window);
 
         glfwPollEvents();
     }
+    glDeleteBuffers(1, &vertexBufferObject);
 
     glfwTerminate();
     return 0;
