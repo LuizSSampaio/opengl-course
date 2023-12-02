ebc448f73f02157bcafbd5780a51752a1a2fb8c5
Luiz Henrique Silva Sampaio <luizhsampaio07@gmail.com>
2023-12-02 12:24:41 -0300

    Added MVP

--- src/Application.cpp
+++ src/Application.cpp
@@ -60,6 +60,10 @@ int main() {
     vertexArray.Bind();
 
     const glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
+    const glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(-100, 0, 0));
+    const glm::mat4 model = glm::translate(glm::mat4(1.0f), glm::vec3(200, 200, 0));
+
+    const glm::mat4 mvp = proj * view * model;
 
     const IndexBuffer indexBuffer(indices, 6);
 
@@ -67,7 +71,7 @@ int main() {
     shader.Bind();
 
     shader.SetUniform4f("u_Color",0.2f, 0.3f, 0.8f, 1.0f);
-    shader.SetUniformMat4f("u_MVP", proj);
+    shader.SetUniformMat4f("u_MVP", mvp);
 
     const Texture texture("../resources/textures/pop_cat.png");
     texture.Bind();
